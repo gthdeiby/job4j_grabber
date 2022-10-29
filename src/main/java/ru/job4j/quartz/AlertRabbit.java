7f130fdce2f51448da21b8646f903b3a9da585cf
dim0nster <32572277+dim0nster@users.noreply.github.com>
2022-10-29 19:21:59 +0300

    1.1. Job c параметрами [#260360]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,6 +3,11 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -23,30 +28,79 @@ public class AlertRabbit {
         return properties;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Properties properties = loadProperties();
         int interval = Integer.parseInt(properties.getProperty("rabbit.interval"));
-        try {
+        Class.forName(properties.getProperty("jdbc.driver"));
+        try (Connection connection = DriverManager.getConnection(
+                    properties.getProperty("jdbc.url"),
+                    properties.getProperty("jdbc.username"),
+                    properties.getProperty("jdbc.password")
+            )) {
+            /**
+             *  Создание класса, управляющего всеми работами.
+             *  В объект Scheduler будут добавляться задачи.
+             */
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            /**
+             * Создание объекта для передачи в Job при старте.
+             */
+            JobDataMap data = new JobDataMap();
+            data.put("connection", connection);
+            /**
+             * Создание задачи.
+             */
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
+            /**
+             * Создание расписания.
+             */
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(interval)
                     .repeatForever();
+            /**
+             * Задача выполняется через триггер.
+             * Указывается, когда и с каким расписанием производить запуск.
+             */
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
+            /**
+             * Загрузка задачи и триггера в планировщик.
+             */
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            /**
+             * Завершение работы.
+             */
+            Thread.sleep(10000);
+            scheduler.shutdown();
+
+        } catch (Exception se) {
             se.printStackTrace();
         }
     }
 
     public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
         public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
+            Connection cn = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+            try (PreparedStatement ps = cn.prepareStatement(
+                    "insert into rabbit (created_date) values (?)"
+            )) {
+                ps.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+                ps.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
