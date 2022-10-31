dff52997c3c899cf48a4866c94ee8647913ad29b
dim0nster <32572277+dim0nster@users.noreply.github.com>
2022-10-31 07:30:04 +0300

    2.2. Модель данных - Post. [#285211] fix

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -79,11 +79,11 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return Objects.equals(link, post.link);
+        return id == post.id && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(link);
+        return Objects.hash(id, link);
     }
 }
