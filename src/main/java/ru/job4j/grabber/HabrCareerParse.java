249e38ef520fbf02a7dd840e9f5b84b875442cc7
dim0nster <32572277+dim0nster@users.noreply.github.com>
2022-10-31 05:52:45 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1 [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -15,17 +15,19 @@ public class HabrCareerParse {
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            Element dateElement = row.select(".basic-date").first();
-            String vacancyName = titleElement.text();
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            String date = String.format("%s", dateElement.attr("datetime"));
-            System.out.printf("%s%n %s %s%n", date, vacancyName, link);
-        });
+        for (int page = 1; page <= 5; page++) {
+            Connection connection = Jsoup.connect(String.format("%s?page=%d", PAGE_LINK, page));
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                Element dateElement = row.select(".basic-date").first();
+                String vacancyName = titleElement.text();
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                String date = String.format("%s", dateElement.attr("datetime"));
+                System.out.printf("%s%n%s %s%n", date, vacancyName, link);
+            });
+        }
     }
 }
