805075e3fb125f576106792f4a77917ac6ca476c
dim0nster <32572277+dim0nster@users.noreply.github.com>
2022-10-31 17:55:57 +0300

    2.2. Модель данных - Post. [#285211 #356460]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -9,10 +9,17 @@ public class Post {
     private String title;
     private String link;
     private String description;
-    LocalDateTime created;
+    private LocalDateTime created;
 
     public Post(int id, String title, String link, String description, LocalDateTime created) {
         this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
+    public Post(String title, String link, String description, LocalDateTime created) {
         this.title = title;
         this.link = link;
         this.description = description;
